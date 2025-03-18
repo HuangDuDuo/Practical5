43f7988204e490eea538f579a121c675843acd0e
Apple <huangduduo@hotmail.com>
2025-03-18 16:25:14 +0800

    programming_language_popularity.py

---
+++ programming_language_popularity.py
@@ -0,0 +1,14 @@
+my_dict = {}
+Lan = {'JavaScript':62.3, 'HTML':52.9, 'Python':51, 'SQL':51, 'TypeScript':38.5}
+print (Lan)
+import matplotlib
+import matplotlib.pyplot as plt
+language = list(Lan.keys())
+values = list(Lan.values())
+plt.bar(language,values)
+plt.title('Programming language popularity')
+plt.xlabel('Language')
+plt.ylabel('Users (percentage)')
+plt.show
+a = input("Input language:")
+print ("The percentage of developers who use", a, "is", Lan[a], ".")
