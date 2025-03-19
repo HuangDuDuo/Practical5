c9e5c28b4b19d93a1d2900c1c0bb5dfb8806a515
Apple <huangduduo@hotmail.com>
2025-03-19 15:05:00 +0800

    final

--- population_sizes.py
+++ population_sizes.py
@@ -1,6 +1,9 @@
-
+#get the lists and print in order
 uk_countries = [57.11,3.13,1.91,5.45]
 China_provinces = [65.77,41.88,45.28,61.27,85.15]
+print ("sorted for UK:", sorted(uk_countries))
+print ("sorted for China:", sorted(China_provinces))
+#draw pie plot 1
 import matplotlib.pyplot as plt
 labels1 = 'England', 'Wales', 'Northern Ireland', 'Scotland'
 sizes = list(uk_countries)
@@ -8,7 +11,7 @@ plt.title('Population sizes of UK countries')
 plt.pie(sizes,labels=labels1, autopct='%1.1f%%', startangle=90)
 plt.axis('equal')
 plt.show()
-
+#draw pie plot 2
 labels2 = 'Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu'
 sizes = list(China_provinces)
 plt.title('Population sizes of China provinces')
--- programming_language_popularity.py
+++ programming_language_popularity.py
@@ -1,14 +1,17 @@
-#No show!!
+#get dictionary and print
 Lan = {'JavaScript':62.3, 'HTML':52.9, 'Python':51, 'SQL':51, 'TypeScript':38.5}
 print (Lan)
 import matplotlib
 import matplotlib.pyplot as plt
+#set x-axis and y-axis
 language = list(Lan.keys())
 values = list(Lan.values())
+#draw bar plot
 plt.bar(language,values)
 plt.title('Programming language popularity')
 plt.xlabel('Language')
 plt.ylabel('Users (percentage)')
 plt.show()
+#print percentage
 a = input("Input language:")
 print ("The percentage of developers who use", a, "is", Lan[a], ".")
